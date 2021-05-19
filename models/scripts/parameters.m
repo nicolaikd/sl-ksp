bfb91f23b9ed8244619560a7da09318e8fe12b44
DESKTOP-7U6FG5S\Nicolai <nicolai.kd.abb@outlook.dk>
2021-05-19 21:26:11 +0200

    1) Updates model to subsystem in mass estimater. 2) Parameter.m script updated.

--- models/scripts/parameters.m
+++ models/scripts/parameters.m
@@ -2,9 +2,9 @@
 
 % Model parameters
 
-P_m_rocket          = 1000;     %[kg]       Mass of rocket
-P_Cd_rocket         = 0.2;      %[-]        Rocket drag coefficient
-P_A_front_rocket    = 1;        %[m^2]   	Rocket drag coefficient
+P_m_rocket          = 1000;             %[kg]       Mass of rocket
+P_Cd_rocket         = 0.2;              %[-]        Rocket drag coefficient
+P_A_front_rocket    = (2.6/2)^2 * pi;   %[m^2]   	Rocket drag coefficient  (assumed front of rocket is a cicle): r^2 * pi   
 
 %%
 
