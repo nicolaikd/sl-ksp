851f912b207652d4900c035e685499ab0f13cc3b
ldaugb99 <lassekd99@live.dk>
2021-06-01 22:42:34 +0200

    Added time to zero vertical velocity calculation module

--- models/scripts/parameters.m
+++ models/scripts/parameters.m
@@ -6,6 +6,9 @@ P_m_rocket          = 1000;             %[kg]       Mass of rocket
 P_Cd_rocket         = 0.2;              %[-]        Rocket drag coefficient
 P_A_front_rocket    = (2.6/2)^2 * pi;   %[m^2]   	Rocket drag coefficient  (assumed front of rocket is a cicle): r^2 * pi   
 
-%%
+%% Mass calculator
+P_mc_fuel_rocket    = 5776;             % [amount]  Amount of liquid fuel at launch
+P_mc_wet_mass_rocket= 101.197;          % [tons]    Wet mass of rocket - Mass of rocket and fuel at launch
+P_mc_dry_mass_rocket=37.397;            % [tons]    Dry mass of rocket - Mass of rocket with no fuel
 
 
